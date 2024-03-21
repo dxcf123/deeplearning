a0d87c9a32a11b0c715063d730bea16bb849c5c6
dxcf123 <103616949+dxcf123@users.noreply.github.com>
2024-03-21 10:34:48 +0800

    Update classmodel.py

--- code1/使用普通的卷积神经网络分类MNIST/classmodel.py
+++ code1/使用普通的卷积神经网络分类MNIST/classmodel.py
@@ -4,12 +4,6 @@ from torch import nn
 
 class Model(nn.Module):  # 构建全连接层网络
     def __init__(self):
-        """
-        :param input_: 输入维度
-        :param hidden: 隐藏维度
-        :param output_: 输出维度
-        :param num: 隐藏层数量减1，本网络至少1层隐藏层，即当num=0时，代表1层隐藏层
-        """
         super(Model, self).__init__()
         # 输入通道，输出通道，卷积核大小，步长，填充
         self.cov1 = nn.Conv2d(1, 10, 3, stride=1, padding=1)
@@ -27,6 +21,6 @@ class Model(nn.Module):  # 构建全连接层网络
         x = self.cov2(x)  # 第二层卷积 14→14
         x = self.maxpool2(x)  # 第二层池化 14→7
         x = torch.relu(x)  # 激活函数
-        x = x.view(x.size(0), -1)  # 将特征展平 7*7→49
-        x = self.lin1(x)  # 全连接层 49→10
+        x = x.view(x.size(0), -1)  # 将特征展平 10*7*7→490
+        x = self.lin1(x)  # 全连接层 490→10
         return x
